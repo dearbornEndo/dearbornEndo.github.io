9dd5503584081f6b701e410f009bd9bfcf34e3ae
Basel Saleh <bsaleh03@gmail.com>
2020-06-09 16:33:47 -0400

    Updates

---
+++ component---src-pages-contact-success-index-js-62888348cf42ebe0d93a.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{aWcV:function(e,t,a){"use strict";a.r(t);var s=a("q1tI"),c=a.n(s),n=a("Zttt");t.default=function(){return c.a.createElement(n.a,null,c.a.createElement("section",{className:"hero is-primary is-bold is-large"},c.a.createElement("div",{className:"hero-body has-text-centered"},c.a.createElement("h1",{className:"title"},"Success"))))}}}]);
+//# sourceMappingURL=component---src-pages-contact-success-index-js-62888348cf42ebe0d93a.js.map
